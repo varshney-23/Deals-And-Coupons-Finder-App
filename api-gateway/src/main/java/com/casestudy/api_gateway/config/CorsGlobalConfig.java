faeae7cb8c9a1183d7d70acc39cabe59d11b82a2
varshney-23 <ayushivarshney40@gmail.com>
2025-07-14 22:46:01 +0530

    updated with brands

---
+++ api-gateway/src/main/java/com/casestudy/api_gateway/config/CorsGlobalConfig.java
@@ -0,0 +1,26 @@
+package com.casestudy.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // ✅ Avoid multiple origins
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
--- payment-service/src/main/java/com/casestudy/payment_service/model/PayPojo.java
+++ payment-service/src/main/java/com/casestudy/payment_service/model/PayPojo.java
@@ -2,10 +2,14 @@ package com.casestudy.payment_service.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 
 @Data
 @Entity
+@Getter
+@Setter
 @Table(name = "payments")
 public class PayPojo {
 
